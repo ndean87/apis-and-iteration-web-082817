860a111682bd095120d7d18d8bd6de843ee46b96
github username <github email address>
2017-08-29 17:33:07 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -24,23 +24,6 @@ def get_character_movies_from_api(character)
 
   end
 
-
-
-  # character_hash["results"].each do |character_profile|
-  #   if character_profile["name"] == character
-  #     character_hash["results"][character_profile]["films"]
-  #   end
-  #end
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
-
 end
 
 def parse_character_movies(films_hash)
@@ -68,11 +51,3 @@ def show_character_movies(character)
     parse_character_movies(films_hash)
   end
 end
-
-# get_character_movies_from_api("Luke Skywalker")
-
-# parse_character_movies("sdferdf")
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
